90d8d73b1bb0a3dbf7b333449ce589b35f930c79
Begin Again <101597204+the-ashen-one@users.noreply.github.com>
2022-11-27 17:12:09 +0600

    added JSON fetch and parse

--- stonks  ͡° ͜ʖ ͡°/Controllers/ViewController.swift
+++ stonks  ͡° ͜ʖ ͡°/Controllers/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let baseURL = "https://www.cbr-xml-daily.ru/latest.js"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.fetchJSON(urlStr: baseURL) { (result) in
+            switch result {
+            case .success(let data): self.parse(JSON: data)
+            case .failure(let error): print(error)
+            }
+        }
+        
     }
 
+    private func parse(JSON: Data) {
+        do {
+            let decoder = JSONDecoder()
+            let currenciesWR = try decoder.decode(CurrenciesWR.self, from: JSON)
+        } catch {
+            print("decoding failed")
+        }
+    }
+    
+    private func fetchJSON(urlStr: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        if let url = URL(string: urlStr) {
+            let urlSession = URLSession(configuration: .default).dataTask(with: url)
+            { (data, response, error) in
+                if let error = error {
+                    completion(.failure(error))
+                }
+                if let safeData = data {
+                    completion(.success(safeData))
+                }
+            }
+            urlSession.resume()
+        }
+    }
 
 }
 
--- stonks  ͡° ͜ʖ ͡°/Models/CurrenciesWithRates.swift
+++ stonks  ͡° ͜ʖ ͡°/Models/CurrenciesWithRates.swift
@@ -8,7 +8,23 @@
 import Foundation
 
 struct CurrenciesWR: Decodable {
-
+    let rates: [String: Double]?
     
+    var currencies: [String]?
+    mutating func addCurr(from keys: String) {
+        if rates != nil {
+            for key in rates!.keys {
+                currencies?.append(key)
+            }
+        }
+    }
     
+    var ratesToRub: [Double]?
+    mutating func addRates(from values: Double) {
+        if rates != nil {
+            for value in rates!.values {
+                ratesToRub?.append(value)
+            }
+        }
+    }
 }
